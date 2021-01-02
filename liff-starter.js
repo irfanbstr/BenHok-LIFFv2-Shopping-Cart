6b4c47267f1ab2cb05618e37b8c48ddf79d75456
irfanbstr <irfanbstr@gmail.com>
2021-01-02 07:36:45 +0700

    test fungsi3

--- liff-starter.js
+++ liff-starter.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const useNodeJS = true;   // if you are not using a node server, set this value to false
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
     const defaultLiffId = "1655333814-dvlkZqOv";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
